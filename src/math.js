9f3996cb749eb8620de93709607c60978acc0a78
Russian-Wolf <mishabolshacov@yandex.ru>
2026-03-12 18:57:25 +0300

    fix isPrime logic

--- src/math.js
+++ src/math.js
@@ -72,7 +72,7 @@ export const isPrime = (number) => {
   if (number < 2) return false
   if (number === 2) return true
   if (number % 2 === 0) return false
-  for (let i = 3; i < Math.sqrt(number); i += 2) {
+  for (let i = 3; i <= Math.sqrt(number); i += 2) {
     if (number % i === 0) return false
   }
   return true
